e5abee3abcb85685b30e8e3792d358a957af9895
Michael Ross <michael.s.ross@protonmail.com>
2023-12-05 15:30:57 -0700

    Refactor spawned task into a pull system function

--- src/main.rs
+++ src/main.rs
@@ -33,41 +33,46 @@ struct Position {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>>{
     let client = Client::new();
+    let system_ids = get_system_ids(&client).await.unwrap();
+
+    let graph = get_graph_client().await;
+
+    let system_pulls: Vec<_> = system_ids
+        .iter()
+        .map(|&system_id| tokio::spawn(pull_system(client.clone(), graph.clone(), system_id)))
+        .collect();
+
+    futures::future::try_join_all(system_pulls).await?;
+
+    Ok(())
+}
+
+async fn pull_system(
+    client: Client,
+    graph: Arc<Graph>,
+    system_id: i64,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    match get_system_details(&client, system_id).await {
+        Ok(system_details) => {
+            if let Err(err) = save_system_to_neo4j(&graph, &system_details).await {
+                eprintln!("Error saving system {}: {}", system_details.system_id, err);
+            } else {
+                print!("{:?}, ", system_details.system_id);
+            }
+        }
+        Err(err) => {
+            eprintln!("Error getting system details for system {}: {}", system_id, err);
+        }
+    }
+
+    Ok(())
+}
+
+async fn get_graph_client() -> Arc<Graph> {
     let uri = "bolt://localhost:7687";
     let user = "neo4j";
     let pass = "neo4jneo4j"; // assumes you have accessed via the browser and updated pass
-
-    let graph = Arc::new(Graph::new(uri, user, pass).await.unwrap());
-
-    let system_ids = get_system_ids(&client).await.unwrap();
-    let mut handles = vec![];
-
-    for system_id in system_ids {
-        let client_clone = client.clone(); // Clone the client before moving it into the async block
-        let graph = graph.clone();
-        let handle = tokio::spawn(async move {
-            match get_system_details(&client_clone, system_id).await {
-                Ok(system_details) => {
-                    if let Err(err) = save_system_to_neo4j(&graph, &system_details).await {
-                        eprintln!("Error saving system {}: {}", system_details.system_id, err);
-                    } else {
-                        print!("{:?}, ", system_details.system_id);
-                    }
-                }
-                Err(err) => {
-                    eprintln!("Error getting system details for system {}: {}", system_id, err);
-                }
-            }
-
-            Ok::<(), Box<dyn std::error::Error + Send + Sync>>(())
-        });
-
-        handles.push(handle);
-    }
-
-    futures::future::try_join_all(handles).await?;
-
-    Ok(())
+    Arc::new(Graph::new(uri, user, pass).await.unwrap())
 }
 
 async fn get_system_details(client: &Client, system_id: i64) -> Result<System, reqwest::Error> {
