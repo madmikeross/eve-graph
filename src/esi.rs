12934644b75919e27ffe7d3aa025fa519264b926
Mike Ross <4534713+madmikeross@users.noreply.github.com>
2023-12-15 11:44:42 -0700

    Build and test with GitHub Actions (#3)

--- src/esi.rs
+++ src/esi.rs
@@ -3,7 +3,7 @@ use reqwest::{Client, Response};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize)]
-pub(crate) struct SystemEsiResponse {
+pub struct SystemEsiResponse {
     pub constellation_id: Option<i64>,
     pub name: Option<String>,
     pub planets: Option<Vec<Planet>>,
@@ -16,21 +16,21 @@ pub(crate) struct SystemEsiResponse {
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub(crate) struct Planet {
+pub struct Planet {
     pub planet_id: i64,
     pub asteroid_belts: Option<Vec<i64>>,
     pub moons: Option<Vec<i64>>,
 }
 
 #[derive(Debug, Deserialize)]
-pub(crate) struct Position {
+pub struct Position {
     pub x: f64,
     pub y: f64,
     pub z: f64,
 }
 
 #[derive(Debug, Deserialize)]
-pub(crate) struct StargateEsiResponse {
+pub struct StargateEsiResponse {
     pub destination: Destination,
     pub name: String,
     pub position: Position,
@@ -40,12 +40,12 @@ pub(crate) struct StargateEsiResponse {
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-pub(crate) struct Destination {
+pub struct Destination {
     pub stargate_id: i64,
     pub system_id: i64,
 }
 
-pub(crate) async fn get_system_details(
+pub async fn get_system_details(
     client: &Client,
     system_id: i64,
 ) -> Result<SystemEsiResponse, reqwest::Error> {
@@ -57,7 +57,7 @@ pub(crate) async fn get_system_details(
     response.json().await
 }
 
-pub(crate) async fn get_stargate_details(
+pub async fn get_stargate_details(
     client: &Client,
     stargate_id: i64,
 ) -> Result<StargateEsiResponse, reqwest::Error> {
@@ -69,28 +69,27 @@ pub(crate) async fn get_stargate_details(
     response.json().await
 }
 
-pub(crate) async fn get_system_ids(client: &Client) -> Result<Vec<i64>, reqwest::Error> {
+pub async fn get_system_ids(client: &Client) -> Result<Vec<i64>, reqwest::Error> {
     let systems_url = "https://esi.evetech.net/latest/universe/systems/";
     let response = client.get(systems_url).send().await?;
     response.json().await
 }
+
 #[derive(Debug)]
-pub(crate) struct SystemKillsResponse {
-    last_modified: Option<DateTime<Utc>>,
+pub struct SystemKillsResponse {
+    pub last_modified: Option<DateTime<Utc>>,
     pub system_kills: Vec<SystemKills>,
 }
 
 #[derive(Debug, Deserialize)]
-pub(crate) struct SystemKills {
+pub struct SystemKills {
     npc_kills: i64,
     pod_kills: i64,
     pub ship_kills: i32,
     pub system_id: i64,
 }
 
-pub(crate) async fn get_system_kills(
-    client: &Client,
-) -> Result<SystemKillsResponse, reqwest::Error> {
+pub async fn get_system_kills(client: &Client) -> Result<SystemKillsResponse, reqwest::Error> {
     let system_kills_url = "https://esi.evetech.net/latest/universe/system_kills/";
     let response = client.get(system_kills_url).send().await?;
     let last_modified = get_last_modified_date(&response);
@@ -118,20 +117,18 @@ fn get_last_modified_date(response: &Response) -> Option<DateTime<Utc>> {
 }
 
 #[derive(Debug)]
-pub(crate) struct SystemJumpsResponse {
-    last_modified: Option<DateTime<Utc>>,
+pub struct SystemJumpsResponse {
+    pub last_modified: Option<DateTime<Utc>>,
     pub system_jumps: Vec<SystemJumps>,
 }
 
 #[derive(Debug, Deserialize)]
-pub(crate) struct SystemJumps {
+pub struct SystemJumps {
     pub ship_jumps: i32,
     pub system_id: i64,
 }
 
-pub(crate) async fn get_system_jumps(
-    client: &Client,
-) -> Result<SystemJumpsResponse, reqwest::Error> {
+pub async fn get_system_jumps(client: &Client) -> Result<SystemJumpsResponse, reqwest::Error> {
     let system_jumps_url = "https://esi.evetech.net/latest/universe/system_jumps/";
     let response = client.get(system_jumps_url).send().await?;
     let last_modified = get_last_modified_date(&response);
@@ -140,40 +137,4 @@ pub(crate) async fn get_system_jumps(
         last_modified,
         system_jumps,
     })
-}
-
-#[cfg(test)]
-mod tests {
-    use reqwest::Client;
-
-    use crate::esi::{get_stargate_details, get_system_jumps, get_system_kills};
-
-    #[tokio::test]
-    async fn should_get_system_kills() {
-        let client = Client::new();
-        let system_kills_response = get_system_kills(&client).await.unwrap();
-
-        assert!(&system_kills_response.last_modified.is_some());
-        assert!(&system_kills_response.system_kills.len() > &0);
-    }
-
-    #[tokio::test]
-    async fn should_get_system_jumps() {
-        let client = Client::new();
-        let system_jumps_response = get_system_jumps(&client).await.unwrap();
-
-        assert!(&system_jumps_response.last_modified.is_some());
-        assert!(&system_jumps_response.system_jumps.len() > &0);
-    }
-
-    #[tokio::test]
-    async fn should_get_stargate_details() {
-        let client = Client::new();
-        let stargate_id = 50011905;
-
-        let stargate = get_stargate_details(&client, stargate_id).await.unwrap();
-
-        assert_eq!(stargate.stargate_id, stargate_id);
-        assert_eq!(stargate.name, "Stargate (Vouskiaho)");
-    }
 }
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod database;
+pub mod esi;
+pub mod eve_scout;
